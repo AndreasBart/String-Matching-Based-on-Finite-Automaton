631cb9c5c4a25696b54ec54f263eda86fb4a3081
Andreas <bart2604@web.de>
2021-05-21 16:57:17 +0200

    H04

--- src/App.java
+++ src/App.java
@@ -2,10 +2,15 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         List<Character> liste = new ArrayList<Character>(){}; liste.add('x'); liste.add('s'); liste.add('x');
-        UnicodeNumberOfCharIndex u = new UnicodeNumberOfCharIndex();
+
         SelectionOfCharsIndex s = new SelectionOfCharsIndex(liste);
         
         System.out.println(s.sizeOfAlphabet());
+        System.out.println(s.apply('s'));
+
+        
+
     }
 }
